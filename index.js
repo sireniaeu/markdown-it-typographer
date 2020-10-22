6a914c19c59eccae96f72ed7ab3fa29c7f97d8b4
Jonathan Bunde-Pedersen <jonathan@purebadger.com>
2020-10-22 10:08:03 +0200

    Linting

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 /*
  * Mostly copied from:
@@ -26,14 +26,11 @@
 // - fractionals 1/2, 1/4, 3/4 -> ½, ¼, ¾
 // - miltiplication 2 x 4 -> 2 × 4
 
-
 // Convert straight quotation marks to typographic ones
 //
 
-
-
-function isWhiteSpace(code) {
-  if (code >= 0x2000 && code <= 0x200A) { return true; }
+function isWhiteSpace (code) {
+  if (code >= 0x2000 && code <= 0x200A) { return true }
   switch (code) {
     case 0x09: // \t
     case 0x0A: // \n
@@ -46,19 +43,19 @@ function isWhiteSpace(code) {
     case 0x202F:
     case 0x205F:
     case 0x3000:
-      return true;
+      return true
   }
-  return false;
+  return false
 }
 
-var UNICODE_PUNCT_RE = require('uc.micro/categories/p/regex');
+var UNICODE_PUNCT_RE = require('uc.micro/categories/p/regex')
 
 // Currently without astral characters support.
-function isPunctChar(ch) {
-  return UNICODE_PUNCT_RE.test(ch);
+function isPunctChar (ch) {
+  return UNICODE_PUNCT_RE.test(ch)
 }
 
-function isMdAsciiPunct(ch) {
+function isMdAsciiPunct (ch) {
   switch (ch) {
     case 0x21/* ! */:
     case 0x22/* " */:
@@ -92,115 +89,114 @@ function isMdAsciiPunct(ch) {
     case 0x7C/* | */:
     case 0x7D/* } */:
     case 0x7E/* ~ */:
-      return true;
+      return true
     default:
-      return false;
+      return false
   }
 }
 
-var QUOTE_TEST_RE = /['"]/;
-var QUOTE_RE = /['"]/g;
-var APOSTROPHE = '\u2019'; /* ’ */
+var QUOTE_TEST_RE = /['"]/
+var QUOTE_RE = /['"]/g
+var APOSTROPHE = '\u2019' /* ’ */
 
-
-function replaceAt(str, index, ch) {
-  return str.substr(0, index) + ch + str.substr(index + 1);
+function replaceAt (str, index, ch) {
+  return str.substr(0, index) + ch + str.substr(index + 1)
 }
 
-function process_inlines(tokens, state) {
+function process_inlines (tokens, state) {
   var i, token, text, t, pos, max, thisLevel, item, lastChar, nextChar,
-      isLastPunctChar, isNextPunctChar, isLastWhiteSpace, isNextWhiteSpace,
-      canOpen, canClose, j, isSingle, stack, openQuote, closeQuote;
+    isLastPunctChar, isNextPunctChar, isLastWhiteSpace, isNextWhiteSpace,
+    canOpen, canClose, j, isSingle, stack, openQuote, closeQuote
 
-  stack = [];
+  stack = []
 
   for (i = 0; i < tokens.length; i++) {
-    token = tokens[i];
+    token = tokens[i]
 
-    thisLevel = tokens[i].level;
+    thisLevel = tokens[i].level
 
     for (j = stack.length - 1; j >= 0; j--) {
-      if (stack[j].level <= thisLevel) { break; }
+      if (stack[j].level <= thisLevel) { break }
     }
-    stack.length = j + 1;
+    stack.length = j + 1
 
-    if (token.type !== 'text') { continue; }
+    if (token.type !== 'text') { continue }
 
-    text = token.content;
-    pos = 0;
-    max = text.length;
+    text = token.content
+    pos = 0
+    max = text.length
 
-    /*eslint no-labels:0,block-scoped-var:0*/
+    /* eslint no-labels:0,block-scoped-var:0 */
     OUTER:
     while (pos < max) {
-      QUOTE_RE.lastIndex = pos;
-      t = QUOTE_RE.exec(text);
-      if (!t) { break; }
+      QUOTE_RE.lastIndex = pos
+      t = QUOTE_RE.exec(text)
+      if (!t) { break }
 
-      canOpen = canClose = true;
-      pos = t.index + 1;
-      isSingle = (t[0] === "'");
+      canOpen = canClose = true
+      pos = t.index + 1
+      isSingle = (t[0] === "'")
 
       // Find previous character,
       // default to space if it's the beginning of the line
       //
-      lastChar = 0x20;
+      lastChar = 0x20
 
       if (t.index - 1 >= 0) {
-        lastChar = text.charCodeAt(t.index - 1);
+        lastChar = text.charCodeAt(t.index - 1)
       } else {
         for (j = i - 1; j >= 0; j--) {
-          if (tokens[j].type === 'softbreak' || tokens[j].type === 'hardbreak') break; // lastChar defaults to 0x20
-          if (!tokens[j].content) continue; // should skip all tokens except 'text', 'html_inline' or 'code_inline'
+          if (tokens[j].type === 'softbreak' || tokens[j].type === 'hardbreak') break // lastChar defaults to 0x20
+          if (!tokens[j].content) continue // should skip all tokens except 'text', 'html_inline' or 'code_inline'
 
-          lastChar = tokens[j].content.charCodeAt(tokens[j].content.length - 1);
-          break;
+          lastChar = tokens[j].content.charCodeAt(tokens[j].content.length - 1)
+          break
         }
       }
 
       // Find next character,
       // default to space if it's the end of the line
       //
-      nextChar = 0x20;
+      nextChar = 0x20
 
       if (pos < max) {
-        nextChar = text.charCodeAt(pos);
+        nextChar = text.charCodeAt(pos)
       } else {
         for (j = i + 1; j < tokens.length; j++) {
-          if (tokens[j].type === 'softbreak' || tokens[j].type === 'hardbreak') break; // nextChar defaults to 0x20
-          if (!tokens[j].content) continue; // should skip all tokens except 'text', 'html_inline' or 'code_inline'
+          if (tokens[j].type === 'softbreak' || tokens[j].type === 'hardbreak') break // nextChar defaults to 0x20
+          if (!tokens[j].content) continue // should skip all tokens except 'text', 'html_inline' or 'code_inline'
 
-          nextChar = tokens[j].content.charCodeAt(0);
-          break;
+          nextChar = tokens[j].content.charCodeAt(0)
+          break
         }
       }
 
-      isLastPunctChar = isMdAsciiPunct(lastChar) || isPunctChar(String.fromCharCode(lastChar));
-      isNextPunctChar = isMdAsciiPunct(nextChar) || isPunctChar(String.fromCharCode(nextChar));
+      isLastPunctChar = isMdAsciiPunct(lastChar) || isPunctChar(String.fromCharCode(lastChar))
+      isNextPunctChar = isMdAsciiPunct(nextChar) || isPunctChar(String.fromCharCode(nextChar))
 
-      isLastWhiteSpace = isWhiteSpace(lastChar);
-      isNextWhiteSpace = isWhiteSpace(nextChar);
+      isLastWhiteSpace = isWhiteSpace(lastChar)
+      isNextWhiteSpace = isWhiteSpace(nextChar)
 
       if (isNextWhiteSpace) {
-        canOpen = false;
+        canOpen = false
       } else if (isNextPunctChar) {
         if (!(isLastWhiteSpace || isLastPunctChar)) {
-          canOpen = false;
+          canOpen = false
         }
       }
 
       if (isLastWhiteSpace) {
-        canClose = false;
+        canClose = false
       } else if (isLastPunctChar) {
         if (!(isNextWhiteSpace || isNextPunctChar)) {
-          canClose = false;
+          canClose = false
         }
       }
 
       if (nextChar === 0x22 /* " */ && t[0] === '"') {
         if (lastChar >= 0x30 /* 0 */ && lastChar <= 0x39 /* 9 */) {
           // special case: 1"" - count first quote as an inch
-          canClose = canOpen = false;
+          canClose = canOpen = false
         }
       }
 
@@ -212,49 +208,49 @@ function process_inlines(tokens, state) {
         // 2. foo-"-bar-"-baz - replaced
         // 3. foo"bar"baz     - not replaced
         //
-        canOpen = isLastPunctChar;
-        canClose = isNextPunctChar;
+        canOpen = isLastPunctChar
+        canClose = isNextPunctChar
       }
 
       if (!canOpen && !canClose) {
         // middle of word
         if (isSingle) {
-          token.content = replaceAt(token.content, t.index, APOSTROPHE);
+          token.content = replaceAt(token.content, t.index, APOSTROPHE)
         }
-        continue;
+        continue
       }
 
       if (canClose) {
         // this could be a closing quote, rewind the stack to get a match
         for (j = stack.length - 1; j >= 0; j--) {
-          item = stack[j];
-          if (stack[j].level < thisLevel) { break; }
+          item = stack[j]
+          if (stack[j].level < thisLevel) { break }
           if (item.single === isSingle && stack[j].level === thisLevel) {
-            item = stack[j];
+            item = stack[j]
 
             if (isSingle) {
-              openQuote = state.md.options.quotes[2];
-              closeQuote = state.md.options.quotes[3];
+              openQuote = state.md.options.quotes[2]
+              closeQuote = state.md.options.quotes[3]
             } else {
-              openQuote = state.md.options.quotes[0];
-              closeQuote = state.md.options.quotes[1];
+              openQuote = state.md.options.quotes[0]
+              closeQuote = state.md.options.quotes[1]
             }
 
             // replace token.content *before* tokens[item.token].content,
             // because, if they are pointing at the same token, replaceAt
             // could mess up indices when quote length != 1
-            token.content = replaceAt(token.content, t.index, closeQuote);
+            token.content = replaceAt(token.content, t.index, closeQuote)
             tokens[item.token].content = replaceAt(
-              tokens[item.token].content, item.pos, openQuote);
+              tokens[item.token].content, item.pos, openQuote)
 
-            pos += closeQuote.length - 1;
-            if (item.token === i) { pos += openQuote.length - 1; }
+            pos += closeQuote.length - 1
+            if (item.token === i) { pos += openQuote.length - 1 }
 
-            text = token.content;
-            max = text.length;
+            text = token.content
+            max = text.length
 
-            stack.length = j;
-            continue OUTER;
+            stack.length = j
+            continue OUTER
           }
         }
       }
@@ -265,49 +261,48 @@ function process_inlines(tokens, state) {
           pos: t.index,
           single: isSingle,
           level: thisLevel
-        });
+        })
       } else if (canClose && isSingle) {
-        token.content = replaceAt(token.content, t.index, APOSTROPHE);
+        token.content = replaceAt(token.content, t.index, APOSTROPHE)
       }
     }
   }
 }
 
-
-var RARE_RE = /\+-|\.\.|\?\?\?\?|!!!!|,,|--/;
+var RARE_RE = /\+-|\.\.|\?\?\?\?|!!!!|,,|--/
 
 // Workaround for phantomjs - need regex without /g flag,
 // or root check will fail every second time
-var SCOPED_ABBR_TEST_RE = /\((c|tm|r|p)\)/i;
+var SCOPED_ABBR_TEST_RE = /\((c|tm|r|p)\)/i
 
-var SCOPED_ABBR_RE = /\((c|tm|r|p)\)/ig;
+var SCOPED_ABBR_RE = /\((c|tm|r|p)\)/ig
 var SCOPED_ABBR = {
-  'c': '©',
-  'r': '®',
-  'p': '§',
-  'tm': '™'
-};
-
-function replaceFn(match, name) {
-  return SCOPED_ABBR[name.toLowerCase()];
+  c: '©',
+  r: '®',
+  p: '§',
+  tm: '™'
 }
 
-function replace_scoped(inlineTokens) {
-  var i, token;
+function replaceFn (match, name) {
+  return SCOPED_ABBR[name.toLowerCase()]
+}
+
+function replace_scoped (inlineTokens) {
+  var i, token
 
   for (i = inlineTokens.length - 1; i >= 0; i--) {
-    token = inlineTokens[i];
+    token = inlineTokens[i]
     if (token.type === 'text') {
-      token.content = token.content.replace(SCOPED_ABBR_RE, replaceFn);
+      token.content = token.content.replace(SCOPED_ABBR_RE, replaceFn)
     }
   }
 }
 
-function replace_rare(inlineTokens) {
-  var i, token;
+function replace_rare (inlineTokens) {
+  var i, token
 
   for (i = inlineTokens.length - 1; i >= 0; i--) {
-    token = inlineTokens[i];
+    token = inlineTokens[i]
     if (token.type === 'text') {
       if (RARE_RE.test(token.content)) {
         token.content = token.content
@@ -320,33 +315,31 @@ function replace_rare(inlineTokens) {
           .replace(/(^|[^-])---([^-]|$)/mg, '$1\u2014$2')
           // en-dash
           .replace(/(^|\s)--(\s|$)/mg, '$1\u2013$2')
-          .replace(/(^|[^-\s])--([^-\s]|$)/mg, '$1\u2013$2');
+          .replace(/(^|[^-\s])--([^-\s]|$)/mg, '$1\u2013$2')
       }
     }
   }
 }
 
-function typographer(state) {
+function typographer (state) {
   for (var blkIdx = state.tokens.length - 1; blkIdx >= 0; blkIdx--) {
-
-    if (state.tokens[blkIdx].type !== 'inline') { continue; }
+    if (state.tokens[blkIdx].type !== 'inline') { continue }
 
     if (QUOTE_TEST_RE.test(state.tokens[blkIdx].content)) {
-      process_inlines(state.tokens[blkIdx].children, state);
+      process_inlines(state.tokens[blkIdx].children, state)
     }
 
     if (SCOPED_ABBR_TEST_RE.test(state.tokens[blkIdx].content)) {
-      replace_scoped(state.tokens[blkIdx].children);
+      replace_scoped(state.tokens[blkIdx].children)
     }
 
     if (RARE_RE.test(state.tokens[blkIdx].content)) {
-      replace_rare(state.tokens[blkIdx].children);
+      replace_rare(state.tokens[blkIdx].children)
     }
-
   }
 };
 
-module.exports = function typographer_plugin(md, scheme) {
+module.exports = function typographer_plugin (md, scheme) {
   // After the built-in m-dash and n-dash support
-  md.core.ruler.before('smartquotes', 'typographer', typographer);
-};
+  md.core.ruler.before('smartquotes', 'typographer', typographer)
+}
--- test/test.js
+++ test/test.js
@@ -1,28 +1,26 @@
-'use strict';
+'use strict'
 
-var expect = require('chai').expect;
+var expect = require('chai').expect
 
 describe('markdown-it-typographer', function () {
-  it('should render dashes', function() {
-    var s, target;
-    var md = require('markdown-it')('commonmark').use(require('..'));
+  it('should render dashes', function () {
+    var s, target
+    var md = require('markdown-it')('commonmark').use(require('..'))
 
-    s = '--';
-    target = '<p>\u2013</p>\n';
-    expect(md.render(s)).to.equal(target);
+    s = '--'
+    target = '<p>\u2013</p>\n'
+    expect(md.render(s)).to.equal(target)
 
-    
-    s = 'I am a ---';
-    target = '<p>I am a \u2014</p>\n';
-    expect(md.render(s)).to.equal(target);
-  });
-  it('should render curly quotation marks', function() {
-    var s, target;
-    var md = require('markdown-it')('commonmark').use(require('..'));
+    s = 'I am a ---'
+    target = '<p>I am a \u2014</p>\n'
+    expect(md.render(s)).to.equal(target)
+  })
+  it('should render curly quotation marks', function () {
+    var s, target
+    var md = require('markdown-it')('commonmark').use(require('..'))
 
-    s = 'hello "friend"';
-    target = '<p>hello “friend”</p>\n';
-    expect(md.render(s)).to.equal(target);
-  });
-
-});
+    s = 'hello "friend"'
+    target = '<p>hello “friend”</p>\n'
+    expect(md.render(s)).to.equal(target)
+  })
+})
